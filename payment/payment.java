54ea142385ae4916cc54505bc88b94741c752ba2
Akhil <akhilpandey494@gmail.com>
2024-12-11 18:07:38 +0530

    Hello

---
+++ payment/payment.java
@@ -0,0 +1,9 @@
+package payment;
+public class payment{
+    public static void makepayment(){
+        System.out.println(x:Payment is made sucessfully);
+        public static void main(String[] args){
+            makepayment();
+        }
+    }
+}
